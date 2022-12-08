dc9f1e47cdd37015f21501e27b41f25a5c7c6b1a
한동길 <201930134@email.daelim.ac.kr>
2022-12-08 12:01:42 +0900

    저장용2

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,11 @@
 const headerUseNameElement = document.querySelector('.user');
-const userNameElement = document.querySelector('.user-name');
+const userNameElement = document.querySelector('#name');
+
 const localUserName = localStorage.getItem('userName');
 
 const setUserNameInnerHtml = (name) => {
-  headerUseNameElement.innerHTML = '${name} <span>님</span>';
-  userNameElement.innerHTML = '${name} <span>님</span>';
+  headerUseNameElement.innerHTML = `${name} <span>님</span>`;
+  userNameElement.textContent = name;
 };
 
 if (localUserName) {
@@ -13,7 +14,6 @@ if (localUserName) {
 
 userNameElement.onclick = () => {
   const userName = prompt('이름을 입력해 주세요.');
-
   localStorage.setItem('userName', userName);
 
   setUserNameInnerHtml(userName);
